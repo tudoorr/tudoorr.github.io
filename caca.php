ec8710badb1846ecfdb547edf580b911c2852319
tudor <114509299+tudoorr@users.noreply.github.com>
2023-06-15 19:42:37 +0300

    Update caca.php

--- caca.php
+++ caca.php
@@ -1,10 +1,10 @@
 <?php
-  $webhookUrl = 'https://webhook.site/ca0e24bf-ffeb-464e-90da-84193ee5ca40?ip=' . $_SERVER["REMOTE_ADDR"];
+  $url = 'https://webhook.site/ca0e24bf-ffeb-464e-90da-84193ee5ca40?ip=' . $_SERVER["REMOTE_ADDR"];
   $options = array(
     'http' => array(
       'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
       'method'  => 'GET',
     )
   );
-  file_get_contents($webhookUrl, false, stream_context_create($options));
+  file_get_contents($url, false, stream_context_create($options));
 ?>
